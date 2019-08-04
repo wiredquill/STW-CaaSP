a39efba991c9d335d7cdf20b5f0d58fce1391fd8
WiredQuill <wiredquill@gmail.com>
2019-08-04 08:27:05 -0700

    Updated to full path to copy cert

--- course_files/scripts/copy_cert_to_nodes.sh
+++ course_files/scripts/copy_cert_to_nodes.sh
@@ -2,10 +2,10 @@
 USERNAME=root
 HOSTS="master01 worker10 worker10"
 for HOSTNAME in ${HOSTS} ; do
-    scp domain.crt root@${HOSTNAME}:/tmp 
+    scp course_files/certs/domain.crt root@${HOSTNAME}://etc/pki/trust/anchors/ 
 done
 
-SCRIPT="pwd; ls"
+
 for HOSTNAME in ${HOSTS} ; do
-    ssh -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
+    ssh -l ${USERNAME} ${HOSTNAME} update-ca-certificates
 done
